4d33091e1a46f365da15598398e9772531c69a71
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-07 23:16:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,4 +4,11 @@
 def key_for_min_value(name_hash)
   lowest_value = 0
  lowest_key = nil
+ if lowest_value == 0 || value < lowest_value
+     lowest_value = value 
+     lowest_key = key
+   end
+ end
+ lowest_key
+end
  end
